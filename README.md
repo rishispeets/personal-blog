d1ea478c19b17b0ad36e51b4dda5e22c7446c6f5
rishi <hello@rishispeets.com>
2019-06-30 21:20:23 +0200

    Revert "Revert "Revert "Merge pull request #25 from rishispeets:the-great-cleanse"""
    This reverts commit b6c837c653186774084a9bca099e185ab5e6e7e7.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal blog 
+
+Personal blog using Gatsby. Forked from [gatsby-starter-gcn](https://github.com/ryanwiemer/gatsby-starter-gcn/).
+
+# License
+MIT
---
+++ src/components/Menu.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const Header = styled.header`
+  background: ${props => props.theme.colors.base};
+  width: 100%;
+  padding: 1.5em 0;
+  border-bottom: 1px solid ${props => props.theme.colors.secondary};
+`
+const Nav = styled.nav`
+  width: 100%;
+  max-width: ${props => props.theme.sizes.maxWidth};
+  margin: 0 auto;
+  padding: 0 1.5em;
+
+  ul {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  li {
+    display: inline-block;
+    margin-left: 1em;
+    &:first-child {
+      position: relative;
+      margin: 0;
+      flex-basis: 100%;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: rgb(102, 102, 102);
+    font-weight: 600;
+    transition: all 0.2s;
+    border-bottom: 2px solid ${props => props.theme.colors.base};
+    &:hover {
+      text-decoration-line: underline;
+    }
+  }
+`
+
+const activeLinkStyle = {
+  color: 'darkgrey',
+}
+
+const Menu = () => {
+  return (
+    <Header>
+      <Nav>
+        <ul>
+          <li>
+            <Link to="/" style={{ color: 'black' }}>
+              rishi speets
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects/" activeStyle={activeLinkStyle}>
+              projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/about/" activeStyle={activeLinkStyle}>
+              about
+            </Link>
+          </li>
+          <li>
+            <Link to="/now/" activeStyle={activeLinkStyle}>
+              now
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact/" activeStyle={activeLinkStyle}>
+              contact
+            </Link>
+          </li>
+        </ul>
+      </Nav>
+    </Header>
+  )
+}
+
+export default Menu
