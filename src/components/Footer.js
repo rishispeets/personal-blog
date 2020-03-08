2816906e9c0c84bbc545aeed7f5fa9e35b09cd92
Rishi <hello@rishispeets.com>
2020-03-08 10:25:54 +0100

    Remove placeholders in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,60 +21,33 @@ const List = styled.ul`
   margin: 0 1.5em;
 `
 
-const Item = styled.li`
-  display: inline-block;
-  padding: 0.25em 0;
-  width: 100%;
-  @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    width: auto;
-  }
-  a {
-    font-weight: 600;
-    transition: all 0.2s;
-    color: ${props => props.theme.colors.text};
-    &:hover {
-      color: ${props => props.theme.colors.highlight};
-    }
-    &:visited {
-      color: ${props => props.theme.colors.text};
-    }
-  }
-`
+/*
+ * Use for Item styling when adding footer content.
+ */
+
+// const Item = styled.li`
+//   display: inline-block;
+//   padding: 0.25em 0;
+//   width: 100%;
+//   @media screen and (min-width: ${props => props.theme.responsive.small}) {
+//     width: auto;
+//   }
+//   a {
+//     font-weight: 600;
+//     transition: all 0.2s;
+//     color: ${props => props.theme.colors.text};
+//     &:hover {
+//       color: ${props => props.theme.colors.highlight};
+//     }
+//     &:visited {
+//       color: ${props => props.theme.colors.text};
+//     }
+//   }
+// `
 
 const Footer = () => (
   <Wrapper>
-    <List>
-      <Item>
-        <a
-          href="https://www.contentful.com/"
-          rel="nofollow noopener noreferrer"
-          target="_blank"
-        >
-          <img
-            src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
-            style={{ width: '100px' }}
-            alt="Powered by Contentful"
-          />
-        </a>
-      </Item>
-      <Item>
-        <a
-          href="https://github.com/ryanwiemer/gatsby-starter-gcn"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          gatsby-starter-gcn
-        </a>{' '}
-        by{' '}
-        <a
-          href="https://github.com/ryanwiemer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          @ryanwiemer
-        </a>
-      </Item>
-    </List>
+    <List></List>
   </Wrapper>
 )
 
